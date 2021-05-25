b23819da62d1ba420e9c6b5d20f6f5231ed64b51
inyoung2200 <80299297+inyoung2200@users.noreply.github.com>
2021-05-25 12:39:25 +0900

    2020

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Welcome(props) {
+  return <h1>hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="hong" />
+      <Welcome name="in" />
+      <Welcome name="young" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  App(), document.getElementById('root')
+);
