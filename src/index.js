7f6de554b3577b988c1df1de00657b0a45943e6a
inyoung2200 <80299297+inyoung2200@users.noreply.github.com>
2021-05-25 14:07:51 +0900

    2020

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h1>hello, {props.name}</h1>;
+function formatDate(date){
+  return date.toLocaleDateString();
 }
 
-function App() {
-  return (  
-    <div>
-      <Welcome name="hong" />
-      <Welcome name="in" />
-      <Welcome name="young" />
+function Comment(props){
+  return (
+    <div className="comment">
+      <div className="user-info">
+        <img className="avatar" src={props.author.avatarUrl} alt={props.author.name} />
+        <div className="user-name">{props.author.name}</div>
+      </div>
+      <div className="comment-text">{props.text}</div>
+      <div className="comment-date">{formatDate(props.date)}</div>
     </div>
   );
 }
 
+const comment = {
+  date: new Date(),
+  text: "열심히 할 수록 기회는 따른다.",
+  author: {
+    name: "inyoung",
+    avatarUrl: "http://inyoung2200.dothome.co.kr/portfolio/assets/images/aboutImg02.jpg",
+  },
+};
+
 ReactDOM.render(
-  App(), document.getElementById('root')
+  <Comment date={comment.date} text={comment.text} author={comment.author}/>,
+  document.getElementById("root")
 );
